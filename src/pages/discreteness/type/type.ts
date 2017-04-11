9072e1dc368265c87431092b1571947ca72041ad
duxingxia081 <332931496@qq.com>
2017-04-11 18:13:06 +0800

    商品服务

--- src/pages/discreteness/type/type.ts
+++ src/pages/discreteness/type/type.ts
@@ -1,6 +1,7 @@
-import {Component, ViewChild} from '@angular/core';
-import {NavController, NavParams} from 'ionic-angular';
+import {Component, OnInit} from '@angular/core';
+import {NavController} from 'ionic-angular';
 import {GoodsDetailPage} from "../../goods-detail/goods-detail";
+import {GoodsService} from "../../../providers/goods-service";
 
 /*
   Generated class for the Type page.
@@ -12,12 +13,13 @@ import {GoodsDetailPage} from "../../goods-detail/goods-detail";
   selector: 'page-type',
   templateUrl: 'type.html'
 })
-export class TypePage {
-  //constructor(public nav: NavController,public navCtrl: NavController, public navParams: NavParams) {}
-  constructor(public nav: NavController, public navParams: NavParams) {}
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad TypePage');
+export class TypePage implements OnInit {
+  listGoods:any;
+  constructor(public nav: NavController,private goodsService: GoodsService) {}
+  ngOnInit(): void {
+    this.goodsService.listGoods().then((list) => {
+      this.listGoods = list;
+    });
   }
   goodsDetail(): void {
     this.nav.push(GoodsDetailPage);
